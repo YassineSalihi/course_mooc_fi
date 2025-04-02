fc53de8563b62257b422a1667d7115cd65e421a3
YassineSalihi <y.salihi7457@uca.ac.ma>
2025-04-02 19:47:07 +0000

    using the remove method

---
+++ src/part/partthree/RemoveLast.java
@@ -0,0 +1,33 @@
+package part.partthree;
+
+import java.util.ArrayList;
+
+public class RemoveLast {
+
+    public static void removeLast(ArrayList<String> strings){
+
+        if (strings.isEmpty()) {
+            return;
+        }
+
+        strings.removeLast(); // == strings.remove(strings.size() - 1);
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+
+        removeLast(strings);
+        removeLast(strings);
+
+        System.out.println(strings);
+    }
+
+
+}
